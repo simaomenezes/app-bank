dd2d1027c5ba2107ee45aa8616a080f57d8d88ae
Simão Neto <simaomenezesneto@gmail.com>
2025-01-29 11:05:15 +0000

    [Section-1: Getting Started] chore: Creating simple Spring boot app with out security-part-1.

---
+++ src/main/java/github/com/simaomenezes/appbank/AppBankApplication.java
@@ -0,0 +1,13 @@
+package github.com.simaomenezes.appbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/com/simaomenezes/appbank/AppBankApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.simaomenezes.appbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
