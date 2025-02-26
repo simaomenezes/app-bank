0cffcc75f81f5b271b6cc986d8a9f7e076794d3d
Simão Neto <simaomenezesneto@gmail.com>
2025-02-26 13:42:38 +0000

    [Section-2:Changing the default security configurations] feat: How to disable formlogin and httpbasicauthentication

--- src/main/java/github/com/simaomenezes/appbank/config/ProjectSecurityConfig.java
+++ src/main/java/github/com/simaomenezes/appbank/config/ProjectSecurityConfig.java
@@ -19,6 +19,10 @@ public class ProjectSecurityConfig {
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/myAccount","myBalance","myLoans","myCards").authenticated()
                 .requestMatchers("/notices","/contact","error").permitAll());
+
+        // http.formLogin(flc->flc.disable());
+        // http.httpBasic(hbc->hbc.disable());
+
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
