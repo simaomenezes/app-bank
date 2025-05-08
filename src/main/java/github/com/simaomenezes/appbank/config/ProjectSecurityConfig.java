a5b47eb75ad8f6678403e10c94ebef1ae33ed04b
Simão Neto <simaomenezesneto@gmail.com>
2025-05-08 15:34:11 +0100

    refact: Make only some things

--- src/main/java/github/com/simaomenezes/appbank/config/ProjectSecurityConfig.java
+++ src/main/java/github/com/simaomenezes/appbank/config/ProjectSecurityConfig.java
@@ -2,95 +2,75 @@ package github.com.simaomenezes.appbank.config;
 
 import github.com.simaomenezes.appbank.exceptionhandling.CustomAccessDeniedHandler;
 import github.com.simaomenezes.appbank.exceptionhandling.CustomBasicAuthenticationEntryPoint;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import github.com.simaomenezes.appbank.filter.CsrfCookieFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @Profile("!prod")
 public class ProjectSecurityConfig {
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        // http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-        // http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
-        // http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
-
-        /*
-        http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/myAccount","myBalance","myLoans","myCards").authenticated()
-                .requestMatchers("/notices","/contact","error","/register").permitAll());
-
-        // http.formLogin(flc->flc.disable());
-        // http.httpBasic(hbc->hbc.disable());
-
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
-        return http.build();
-        */
-/*
-        http
-                .requiresChannel(rcc->rcc.anyRequest().requiresInsecure()) // Only HTTP
-                .csrf(csrfConfig -> csrfConfig.disable())
+        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+        http.securityContext(contextConfig -> contextConfig.requireExplicitSave(false))
+                .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(corsConfig -> corsConfig.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
+                .csrf(csrfConfig -> csrfConfig.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+                        .ignoringRequestMatchers("/contact", "/register")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+                .requiresChannel(rcc -> rcc.anyRequest().requiresInsecure()) // Only HTTP
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards", "/user").authenticated()
                         .requestMatchers("/notices", "/contact", "/error", "/register", "/invalidSession").permitAll());
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
-        return http.build();
-
- */
-
-
-
-        http.sessionManagement(smc ->smc.invalidSessionUrl("/invalidSession").maximumSessions(3).maxSessionsPreventsLogin(true))
-                .requiresChannel(rcc ->rcc.anyRequest().requiresInsecure()) // Only HTTP
-                .csrf(csrfConfig -> csrfConfig.disable())
-                .authorizeHttpRequests((requests)->requests
-                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                        .requestMatchers("/notices", "/contact", "/error", "/register", "/invalidSession").permitAll());
-
         http.formLogin(withDefaults());
         http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
         http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
         return http.build();
+
     }
 
-    /*
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{noop}123").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}321").authorities("admin").build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
-    */
-
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 
     /**
      * From Spring Security 6.3 version
-     *
      */
     @Bean
-    public CompromisedPasswordChecker compromisedPasswordChecker(){
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
         return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
--- src/main/java/github/com/simaomenezes/appbank/repository/ContactRepository.java
+++ src/main/java/github/com/simaomenezes/appbank/repository/ContactRepository.java
@@ -3,7 +3,9 @@ package github.com.simaomenezes.appbank.repository;
 import github.com.simaomenezes.appbank.model.Contact;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public interface ContactRepository extends CrudRepository<Contact, String> {
 }
