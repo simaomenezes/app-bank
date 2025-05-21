863ba5098bc360d9bc44afcee30c56258468f4eb
Simão Neto <simaomenezesneto@gmail.com>
2025-05-21 13:24:04 +0100

    feat: Create JWTTokenValidatorFilter, add new model dto to login

---
+++ src/main/java/github/com/simaomenezes/appbank/model/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package github.com.simaomenezes.appbank.model;
+
+public record LoginRequestDTO(String username, String password) {
+}
---
+++ src/main/java/github/com/simaomenezes/appbank/model/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package github.com.simaomenezes.appbank.model;
+
+public record LoginResponseDTO(String status, String jwtToken) {
+}
